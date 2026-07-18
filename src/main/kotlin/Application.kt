a4ddc4da5a0049326cd36f7731d5da469eeff282
Artur Motin <artur.motin98@gmail.com>
2026-07-18 15:53:15 +0300

    Update Application.kt

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,14 +4,23 @@ import io.github.krisalord.config.DatabaseFactory
 import io.github.krisalord.config.loadAppConfig
 import io.github.krisalord.plugins.*
 import io.ktor.server.application.*
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
 
 
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
-}
 
 fun Application.module() {
+    println(">>> SYSTEM: Ktor Module is successfully executing! <<<")
+
+
+    routing {
+        get("/") {
+            call.respondText("The root module is working!")
+        }
+    }
+
     val config = environment.config.loadAppConfig()
 
     DatabaseFactory.init(config.database)
